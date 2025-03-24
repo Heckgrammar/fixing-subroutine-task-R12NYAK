9b8bbfebb07f571c36bb37036205356d0cc1b263
Rainyak <144434306+R12NYAK@users.noreply.github.com>
2025-03-24 23:28:54 +0000

    Update Program.cs

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -4,8 +4,26 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+
+            for(int i = 7 ;i >= 0; i--) {
                 Console.WriteLine(diffCurrencies(i));            
+            }
+        }
+        static string diffCurrencies(int x)
+        {
+            string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
+            if (x >= 0 && x < currencies.Length)
+            {
+                return currencies[1];
+            }
+            else
+            {
+                return "Invalid";
+            }
+
+        }
+    }
+}
             }
         }
         static string diffCurrencies(int x)
